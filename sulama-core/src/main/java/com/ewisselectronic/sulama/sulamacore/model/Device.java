ff359d5610f484115c090d029b0890277fa8834e
MUSTAFA BAHAA SAMISM <mustafa.smesem@gmail.com>
2021-02-02 22:13:07 +0300

    create device table

--- sulama-core/src/main/java/com/ewisselectronic/sulama/sulamacore/model/Device.java
+++ sulama-core/src/main/java/com/ewisselectronic/sulama/sulamacore/model/Device.java
@@ -1,4 +1,28 @@
-package com.ewisselectronic.sulama.sulamacore.model.auth;
+package com.ewisselectronic.sulama.sulamacore.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(indexes = {@Index(columnList = "deleted", name = "INX_DEVICE_deleted")})
 public class Device {
+
+    @Id
+    private Long deviceId;
+    private String deviceName;
+    private Integer roleCount;
+    private Date deviceActivatedDate;
+    private Date deviceLicenceExpiredDate;
+    private Double locationLatitude;
+    private Double locationLongitude;
+    private String deviceLicence;
+    private boolean deleted;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
 }
