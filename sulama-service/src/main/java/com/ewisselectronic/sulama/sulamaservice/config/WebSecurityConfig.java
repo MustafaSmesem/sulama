d1d439b534f39bd96431fa15485790e9152b45b2
MUSTAFA BAHAA SAMISM <mustafa.smesem@gmail.com>
2021-02-02 21:07:29 +0300

    project first upload

--- sulama-service/src/main/java/com/ewisselectronic/sulama/sulamaservice/config/WebSecurityConfig.java
+++ sulama-service/src/main/java/com/ewisselectronic/sulama/sulamaservice/config/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // We don't need CSRF for this example
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/authenticate", "/assets/**", "/public/**")
+                .authorizeRequests().antMatchers("/authenticate", "/assets/**", "/public/**", "/service/**")
                 .permitAll().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().
                 // all other requests need to be authenticated
                 anyRequest().authenticated().and().
